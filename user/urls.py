9dcb05f73d02b7697e34e4dd4f87b54a9b49918d
Eugenio Marconicchio <e.marconicchio@begear.it>
2024-05-15 20:04:53 +0200

    add requirements.txt, add static files directory, use templates for all pages, fix forms, implement login, fix register

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="home"),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
+    path("payroll/", views.payroll, name="payroll"),
 ]
