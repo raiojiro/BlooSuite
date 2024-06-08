769a0d57ce86d24087f037a7fe15c52331583ab2
raiojiro <65910675+raiojiro@users.noreply.github.com>
2024-06-08 18:49:40 +0800

    test

---
+++ scripts/sass.bat
@@ -0,0 +1,3 @@
+cd C:\Users\User\Desktop\BlooSuite\static
+sass style.scss style.css -w
+pause
--- user/admin.py
+++ user/admin.py
@@ -5,3 +5,4 @@ from .models import *
 
 admin.site.register(User)
 admin.site.register(Project)
+admin.site.register(User_group)
